d3a06fc75439c440df846686bbe74ac695ee4bce
VovikMar <vovanmar@gmail.com>
2019-04-27 01:18:21 +0300

    working one

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Home</Link>
+        <Link to="/otherPage">Other Page</Link>
+        </header>
+        <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
+            <Route path="/otherPage" component={OtherPage} />
         </div>
-      </Router>
+      </div>
+     </Router>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const keys = require('./keys');
 
-// Express App Setup
+//Express app setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,62 +9,64 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgres Client Setup
+//Postgress client setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort
+    host: keys.pgHost,
+    port: keys.pgPort,
+    user: keys.pgUser,
+    password: keys.pgPassword,
+    dataase: keys.pgDatabase
+
 });
+
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
 
-// Redis Client Setup
+
+// Redis client setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+
 });
+
 const redisPublisher = redisClient.duplicate();
 
-// Express route handlers
+//Express route handlers
+
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+    res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
-  res.send(values.rows);
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+    redisClient.hgetall('values',(err, values) => {
+        res.send(values);
+    });
 });
 
 app.post('/values', async (req, res) => {
-  const index = req.body.index;
+    const index = req.body.index;
+    if  (parseInt(index) > 40 ){
+        return res.status(422).send('Index too high');
+    }
 
-  if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
-  }
-
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
-  res.send({ working: true });
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert',index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+    res.send({ working: true });
 });
 
 app.listen(5000, err => {
-  console.log('Listening');
+    console.log('Listening');
 });
