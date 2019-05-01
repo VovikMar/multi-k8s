4a73a31dc4c6afccc58919b2bac240707aea279e
VovikMar <vovanmar@gmail.com>
2019-05-01 20:25:06 +0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,19 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/">Home</Link>
-        <Link to="/otherPage">Other Page</Link>
-        </header>
-        <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fib Calculator version 2</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherPage" component={OtherPage} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
-      </div>
-     </Router>
+      </Router>
     );
   }
 }
